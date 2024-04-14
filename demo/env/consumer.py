21738b45508fdf08ceee131a0005ef46d71abe92
nick-mp <nick263mp@gmail.com>
2024-04-14 12:54:03 +0300

    add Readme

--- demo/env/consumer.py
+++ demo/env/consumer.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python
-# coding=utf-8
-import pika
-
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
-channel.queue_declare(queue='hello')
+import pika, sys, os
 
 
-def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='hello')
+
+    def callback(ch, method, properties, body):
+        print(f" [x] Received {body}")
+
+    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
 
 
-channel.basic_consume(callback, queue='hello', no_ack=True)
-channel.start_consuming()
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
